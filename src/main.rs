017643db479e37c61edef647d2e40589979d51c5
David Elkayam <dodlike@gmail.com>
2025-08-12 16:51:15 +0300

    main updated

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-use std::net::SocketAddr;
+use dotenvy;
+use log;
+use pretty_env_logger;
+use sqlx::postgres::PgPoolOptions;
 
 use axum::{
-    routing::{delete, get, post},
     Router,
+    routing::{delete, get, post},
 };
+use log::info;
 
 mod handlers;
 mod models;
+mod persistance;
 
 use handlers::*;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    dotenvy::dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(7)
+        .connect(&std::env::var("DATABASE_URL").expect("env var missing"))
+        .await
+        .expect("Database connection failed!");
+
+
+    // below are simple query test
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .expect("fetching failed");
+
+    info!("********* Question Records *********");
+    info!("Found {} recs", recs.len());
+    info!("{:?}", recs);
+
+    info!("Starting server");
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
