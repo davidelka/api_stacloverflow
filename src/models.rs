e9127d30b1ae0ad2d64a154d394cea0243515709
David Elkayam <dodlike@gmail.com>
2025-08-12 16:51:42 +0300

    add db error

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 #[derive(Serialize, Deserialize)]
 pub struct Question {
@@ -22,13 +23,13 @@ pub struct QuestionId {
 // ----------
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Answer{
+pub struct Answer {
     pub question_uuid: String,
     pub content: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub struct AnswerDetail{
+pub struct AnswerDetail {
     pub answer_uuid: String,
     pub question_uuid: String,
     pub content: String,
@@ -38,4 +39,19 @@ pub struct AnswerDetail{
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AnswerId {
     pub answer_uuid: String,
+}
+
+// ----------
+
+#[derive(Error, Debug)]
+pub enum DBError {
+    #[error("Invalid UUID provided: {0}")]
+    InvalidUUID(String),
+    #[error("Database error occurred")]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+// source: https://www.postgresql.org/docs/current/errcodes-appendix.html
+pub mod postgres_error_codes {
+    pub const FOREIGN_KEY_VIOLATION: &str = "23503";
 }
