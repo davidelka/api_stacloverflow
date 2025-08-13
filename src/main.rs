c63846d2be2d2799bd14a7bc25060eb1dfd110b4
David Elkayam <dodlike@gmail.com>
2025-08-13 13:03:04 +0300

    main update to use innerhandler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate log;
+
 use dotenvy;
-use log;
 use pretty_env_logger;
 use sqlx::postgres::PgPoolOptions;
+
+use std::sync::Arc;
 
 use axum::{
     Router,
@@ -13,7 +17,15 @@ mod handlers;
 mod models;
 mod persistance;
 
+use crate::persistance::answers_dao::{AnswersDao, AnswersDaoImpl};
+use crate::persistance::questions_dao::{QuestionsDao, QuestionsDaoImpl};
 use handlers::*;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -38,13 +50,22 @@ async fn main() {
 
     info!("Starting server");
 
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool.clone()));
+
+    let app_state = AppState {
+        questions_dao,
+        answers_dao,
+    };
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
         .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
