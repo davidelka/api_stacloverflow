04fd4f0ab51f61b8928c691d4cd89b05c34ff52f
David Elkayam <dodlike@gmail.com>
2025-08-13 13:02:26 +0300

    mod update to use innerhandler

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,48 +1,95 @@
+use crate::AppState;
+use crate::handlers::handlers_inner::HandlerError;
 use crate::models::*;
-use axum::{Json, response::IntoResponse};
+use axum::http::StatusCode;
+use axum::{Json, extract::State, response::IntoResponse};
+
 mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
+}
+
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    Json(QuestionDetail {
-        question_uuid: "question_uuid".to_string(),
-        title: "title".to_string(),
-        description: "description".to_string(),
-        created_at: "created_at".to_string(),
-    })
+pub async fn create_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::create_question(question, questions_dao.clone().as_ref()).await;
+
+    match response {
+        Ok(r_question) => Ok(Json(r_question).into_response()),
+        Err(error) => Err(error.into_response()),
+    }
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    Json(vec![QuestionDetail {
-        question_uuid: "question_uuid".to_owned(),
-        title: "title".to_owned(),
-        description: "description".to_owned(),
-        created_at: "created_at".to_owned(),
-    }])
+pub async fn read_questions(
+    State(AppState { questions_dao, .. }): State<AppState>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::read_questions(questions_dao.as_ref()).await;
+
+    match response {
+        Ok(r_questions) => Ok(Json(r_questions).into_response()),
+        Err(error) => Err(error.into_response()),
+    }
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    //..
+pub async fn delete_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::delete_question(question_uuid, questions_dao.as_ref()).await;
+
+    match response {
+        Ok(_) => Ok(()),
+        Err(error) => Err(error.into_response()),
+    }
 }
 
 // ---- CRUD for Answers ----
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    Json(AnswerDetail {
-        answer_uuid: "answer_uuid".to_string(),
-        question_uuid: "question_uuid".to_string(),
-        content: "content".to_string(),
-        created_at: "created_at".to_string(),
-    })
+pub async fn create_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::create_answer(answer, answers_dao.as_ref()).await;
+
+    match response {
+        Ok(r_answer) => Ok(Json(r_answer).into_response()),
+        Err(error) => Err(error.into_response()),
+    }
 }
 
-pub async fn read_answers() -> impl IntoResponse {
-    Json(vec![AnswerDetail {
-        answer_uuid: "answer_uuid".to_string(),
-        question_uuid: "question_uuid".to_string(),
-        content: "content".to_string(),
-        created_at: "created_at".to_string(),
-    }])
+pub async fn read_answers(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::read_answers(question_uuid, answers_dao.as_ref()).await;
+
+    match response {
+        Ok(r_answers) => Ok(Json(r_answers).into_response()),
+        Err(error) => Err(error.into_response()),
+    }
 }
 
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {}
+pub async fn delete_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer_uuid): Json<AnswerId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let response = handlers_inner::delete_answer(answer_uuid, answers_dao.as_ref()).await;
+
+    match response {
+        Ok(_) => Ok(()),
+        Err(error) => Err(error.into_response()),
+    }
+}
